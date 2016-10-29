231dcaa21280ebb33e314e91712a61bedf89ad6c
bigzhu <vermiliondun@gmail.com>
2016-10-29 12:18:31 +0800

    change

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <navigation></navigation>
+  </div>
+</template>
+
+<script>
+  import Navigation from './Navigation'
+  export default {
+    props: [],
+    components: {
+      Navigation
+    },
+    data: function () {
+      return {
+      }
+    },
+    mounted: function () {
+      this.$nextTick(function () {
+        // code that assumes this.$el is in-document
+      })
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+</style>
